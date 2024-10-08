8614872159f06d3fa0e32cf15bc4178a95729502
avdivo <avdivo@mail.ru>
2024-10-08 18:11:56 +0300

    Окончательный вариант с правами доступа

--- finance/views.py
+++ finance/views.py
@@ -1,13 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Category, Transaction
 from .serializers import CategorySerializer, TransactionSerializer
 
 # ViewSet для работы с категориями
 class CategoryViewSet(ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    queryset = Category.objects.all()  # Получаем все категории
+    serializer_class = CategorySerializer  # Указываем сериализатор для категорий
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Разрешаем просмотр всем, запись только аутентифицированным
 
 # ViewSet для работы с транзакциями
 class TransactionViewSet(ModelViewSet):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()  # Получаем все транзакции
+    serializer_class = TransactionSerializer  # Указываем сериализатор для транзакций
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Разрешаем просмотр всем, запись только аутентифицированным
